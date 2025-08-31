940c1806cd4a655f4e9323cd7373e3ca4fb5af0b
InertSloth <229190770+InertSloth@users.noreply.github.com>
2025-08-31 12:50:18 -0400

    extract site-config out of the astro config so changes are clear against one thing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,92 +2,30 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightSidebarTopics from 'starlight-sidebar-topics';
+import { title, description } from './site-config/header';
+import { base, site, source } from './site-config/hosting';
+import { social } from './site-config/social';
+import { defaultLocale, locales } from './site-config/translation';
+import { sidebar } from './site-config/sidebar';
 import isWsl from 'is-wsl';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://apathetic-idleon.github.io',
+	site,
+  	base,
 	integrations: [
 		starlight({
-			title: 'Apathetic Guide to IdleOn',
-			description: 'Gameplay guide to the IdleOn game.',
-			social: [
-				{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/PW6GahZ7'},
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/apathetic-idleon/apathetic-idleon.github.io' }
-			],
+			title,
+			description,
+			defaultLocale,
+			locales,
+			social,
 			editLink: {
-    			baseUrl: 'https://github.com/apathetic-idleon/apathetic-idleon.github.io/edit/main/',
+    			baseUrl: source + '/edit/main/',
   			},
 			lastUpdated: true,
-			plugins: [
-				// icons must be from: https://starlight.astro.build/reference/icons/#all-icons
-				// careful not to use trademarked logos for other purposes
-				starlightSidebarTopics([
-					{
-						id: 'walkthrough',		
-						label: 'Walkthrough',
-						link: 'walkthrough/world/world-1',
-						icon: 'rocket',
-						items: [
-							{
-								label: 'Getting Started',
-								autogenerate: { directory: 'walkthrough/getting-started' },
-							},
-							{
-								label: 'Unlock Worlds',
-								autogenerate: { directory: 'walkthrough/world' },
-							},
-						],
-					},
-					{
-						id: 'mechanic',
-						label: 'Mechanics',
-						link: 'mechanic/global/upgrade-vault',
-						icon: 'setting',
-						items: [
-							{
-								label: 'Global',
-								autogenerate: { directory: 'mechanic/global' },
-							},
-							{
-								label: 'World 1 – Blunder Hills',
-								autogenerate: { directory: 'mechanic/world-1' },
-							},
-							{
-								label: 'World 2 – Yum-Yum Desert',
-								autogenerate: { directory: 'mechanic/world-2' },
-							},
-							{
-								label: 'World 3 – Frostbite Tundra',
-								autogenerate: { directory: 'mechanic/world-3' },
-							},
-							{
-								label: 'World 4 – Hyperion Nebula',
-								autogenerate: { directory: 'mechanic/world-4' },
-							},
-							{
-								label: "World 5 – Smolderin' Plateau",
-								autogenerate: { directory: 'mechanic/world-5' },
-							},
-							{
-								label: 'World 6 – Spirited Valley',
-								autogenerate: { directory: 'mechanic/world-6' },
-							},
-						],
-					},					
-					{
-						id: 'reference',
-						label: 'Reference',
-						link: 'reference/achievements',
-						icon: 'information',
-						items: [
-							{
-								label: 'General',
-								autogenerate: { directory: 'reference' },
-							},
-						],
-					},
-				]),
+			plugins: [				
+				starlightSidebarTopics(sidebar),
 			]
 		}),
 	],
---
+++ site-config/social.ts
@@ -0,0 +1,14 @@
+// Ref: https://starlight.astro.build/reference/configuration/#social
+export const socials = [
+	/// community chat
+	{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/PW6GahZ7'},
+
+	/// code hosting
+	// 		don't feature the code repo in the header,
+	// 		it is mentioned on the contributing page
+	// github
+	// gitlab
+	// bitbucket
+
+	/// more in reference above
+];
