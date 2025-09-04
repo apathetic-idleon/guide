dcf44827d236fe6605f23c7b10b7968de5669abf
InertSloth <229190770+InertSloth@users.noreply.github.com>
2025-09-04 12:56:38 -0400

    remove rehype, fix alias paths

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,6 @@ import starlightSidebarTopics from 'starlight-sidebar-topics';
 import starlightLinksValidator from 'starlight-links-validator';
 import tailwindcss from '@tailwindcss/vite';
 import isWsl from 'is-wsl';
-import rehypeSlug from 'rehype-slug';
 
 // can't use tsconfig aliases yet...
 import { devServerFileWatcher } from './config/integrations/dev-server-file-watcher';
@@ -16,67 +15,56 @@ import { social } from './config/social';
 import { defaultLocale, locales } from './config/translation';
 import { sidebar } from './config/sidebar';
 
-
-
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
-	site,
-	base,
-	trailingSlash: 'always',
-	integrations: [
-		devServerFileWatcher([
-			'./config/**', // Custom plugins and integrations
-		]),
-		starlight({
-			title,
-			description,
-			defaultLocale,
-			locales,
-			social,
-			editLink: {
-				baseUrl: source + '/edit/main/',
-			},
-			lastUpdated: true,
-			components: {
-				Footer: './src/components/Footer.astro'
-			},
-			plugins: [
-				starlightSidebarTopics(sidebar),
-				...(process.env.CHECK_LINKS
-					? [
-							starlightLinksValidator({
-								errorOnFallbackPages: false,
-								errorOnInconsistentLocale: true,
-							}),
-						]
-					: []),
-			],
-			customCss: ['./src/styles/global.css'],
-		}),
-		sitemap(),
-	],
+    output: 'static',
+    site,
+    base,
+    trailingSlash: 'always',
+    integrations: [devServerFileWatcher([
+        './config/**', // Custom plugins and integrations
+		]), starlight({
+        title,
+        description,
+        defaultLocale,
+        locales,
+        social,
+        editLink: {
+            baseUrl: source + '/edit/main/',
+        },
+        lastUpdated: true,
+        components: {
+            Footer: './src/components/Footer.astro'
+        },
+        plugins: [
+            starlightSidebarTopics(sidebar),
+            ...(process.env.CHECK_LINKS
+                ? [
+                        starlightLinksValidator({
+                            errorOnFallbackPages: false,
+                            errorOnInconsistentLocale: true,
+                        }),
+                    ]
+                : []),
+        ],
+        customCss: ['./src/styles/global.css'],
+		}), sitemap()],
 
-	markdown: {
-		// Override with our own config
-		rehypePlugins: [rehypeSlug],
-	},
+    vite: {
+        server: {
+            watch: {
+                // work around WSL bug with /mnt/ watch (could also symlink to linux fs)
+                // https://github.com/withastro/astro/issues/6043#issuecomment-1409498718
+                // https://github.com/microsoft/WSL/issues/4739#issuecomment-2153546812
+                ...(isWsl
+                    ? {
+                            usePolling: true,
+                            interval: 1000,
+                        }
+                    : {}),
+            },
+        },
 
-	vite: {
-		server: {
-			watch: {
-				// work around WSL bug with /mnt/ watch (could also symlink to linux fs)
-				// https://github.com/withastro/astro/issues/6043#issuecomment-1409498718
-				// https://github.com/microsoft/WSL/issues/4739#issuecomment-2153546812
-				...(isWsl
-					? {
-							usePolling: true,
-							interval: 1000,
-						}
-					: {}),
-			},
-		},
-
-		plugins: [tailwindcss()],
-	},
+        plugins: [tailwindcss()],
+    },
 });
