9360a67c16f9bcb8f530ac42793364db95ea6214
InertSloth <229190770+InertSloth@users.noreply.github.com>
2025-09-07 01:26:07 -0400

    footer, usage page

--- config/footer.ts
+++ config/footer.ts
@@ -1,13 +1,16 @@
-export const orgName = 'Apathetic Tools';
+export const org = 'Apathetic Tools';
 export const orgUrl = 'https://github.com/apathetic-tools/';
+export const orgEmail = 'InertSloth@gmail.com'
+
 
 // affiliation
-export const affil = {
-	en: 'Not affiliated in any way with <a href="https://www.legendsofidleon.com/" class="text-inherit">IdleOn</a> &copy; <a href="https://play.google.com/store/apps/dev?id=4698737590260294499" class="text-inherit">LavaFlame2</a>. Images used for commentary only.',
-};
+export const affilGame = 'IdleOn';
+export const affilGameUrl = 'https://www.legendsofidleon.com/';
+export const affilDev = 'LavaFlame2';
+export const affilDevUrl = 'https://play.google.com/store/apps/dev?id=4698737590260294499';
 
 // copyright
 export const copyrightYears = '2025';
 export const license = 'CC BY-NC-SA';
 export const licenseUrl = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
-export const usage = 'using our stuff';
+export const usage = 'using our content';
