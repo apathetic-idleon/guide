7cdd79566d9bd21ce38518e40c337cb45afe91d4
InertSloth <229190770+InertSloth@users.noreply.github.com>
2025-09-07 01:26:07 -0400

    footer, usage page

--- config/hosting.ts
+++ config/hosting.ts
@@ -3,6 +3,7 @@ export const user = process.env.APP_USER ?? 'apathetic-idleon';
 export const repo = process.env.APP_REPO ?? 'apathetic-idleon.github.io';
 export const hostingSlug = user + '/' + repo;
 export const source = process.env.APP_SOURCE ?? 'https://github.com/' + hostingSlug;
+export const sourcePlat = process.env.APP_SOURCE_PLAT ?? 'GitHub';
 
 /// site url
 export const site = process.env.APP_SITE ?? 'https://' + user + '.github.io';
